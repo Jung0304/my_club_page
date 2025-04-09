294cfafeb57bf153c484c221c3fd3367ee6b9fc2
Jung0304 <ynji0403@naver.com>
2025-04-09 22:59:02 +0900

    모바일 최적화

---
+++ script.js
@@ -0,0 +1,11 @@
+function mid_club() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function coll_club() {
+  window.location.href = "coll_club.html";
+}
