643365d3dfea278d6f8b3b22a1ef0d7c46df00b9
Jung0304 <ynji0403@naver.com>
2025-04-12 05:35:26 +0900

    오류없음

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,3 @@
-function block() {
-    document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
-
 function expandButton(el) {
     el.classList.toggle("expanded");
 }
@@ -20,3 +12,28 @@ function selectOnly(btn) {
 function home() {
     window.location.href = "../index.html";
 }
+
+function showClubBox(text,intro) {
+    const box = document.getElementById("content-box");
+    box.innerHTML += `
+        <div class="inner-card">
+        <h3>${text}</h3>
+        <p>${intro}</p>
+        <button onclick="alert('신청됐습니다!')">신청하기</button>
+        </div>
+`;
+}
+
+function midClubBox() {
+    const box = document.getElementById("content-box");
+    box.innerHTML = "";
+    showClubBox('꿈틀이','다빈치 캠퍼스 댄스 중앙동아리입니다.')
+    showClubBox('봉사하자자','다빈치 캠퍼스 볼사 중앙동아리입니다.')
+}
+
+function departClubBox() {
+    const box = document.getElementById("content-box");
+    box.innerHTML = "";
+    showClubBox('그림자찾기','다빈치 캠퍼스 댄스 중앙동아리입니다.')
+    showClubBox('봉사하자자','다빈치 캠퍼스 볼사 중앙동아리입니다.')
+  }
